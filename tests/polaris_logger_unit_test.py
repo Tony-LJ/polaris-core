7a9337912a2680896d3fdabb6f66942494755b63
tony <tony1@kinwong.com>
2025-09-30 14:09:54 +0800

    feat：修改python最低版本限制

--- tests/polaris_logger_unit_test.py
+++ tests/polaris_logger_unit_test.py
@@ -5,3 +5,6 @@ from  polaris_logger import logger
 
 if __name__ == '__main__':
     logger.info(" >>>>>>>>>>>>>>>>>> ")
+    logger.warning(" >>>>>>>>>>>>>>>>>> ")
+    logger.error(" >>>>>>>>>>>>>>>>>> ")
+
