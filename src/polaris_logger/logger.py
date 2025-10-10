17560971e1f72fc85264d5379abb7926faa4650e
zengsg <159675@kinwong.com>
2025-10-10 15:49:01 +0800

    feat:新增字符串处理通用模块

--- src/polaris_logger/logger.py
+++ src/polaris_logger/logger.py
@@ -28,6 +28,11 @@ def setup_file_handler(log_file: str):
     配置文件处理器
     :param log_file:
     :return:
+    作用：配置日志写入文件的规则，使用RotatingFileHandler实现日志滚动：
+    maxBytes=20*1024*1024：单个日志文件最大 20MB，超过则自动切割；
+    backupCount=10：最多保留 10 个历史日志文件（如server.log.1、server.log.2...）；
+    encoding='utf-8'：支持中文日志正常写入。
+    日志级别：setLevel(logging.DEBUG)，即文件会记录所有DEBUG及以上级别的日志（最详细）。
     """
     try:
         file_handler = RotatingFileHandler(
@@ -48,6 +53,9 @@ def setup_console_handler():
     """
     配置控制台处理器
     :return:
+    作用：配置日志在控制台的输出规则：
+    输出目标：控制台（StreamHandler默认输出到stdout）；
+    日志级别：setLevel(logging.INFO)，即控制台只显示INFO及以上级别（过滤掉DEBUG调试信息，避免控制台冗余）。
     """
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(setup_logger_formatter())
@@ -78,18 +86,18 @@ class LoggerSingleton:
 
     def init_logger(self):
         try:
-            basedir = Path(__file__).resolve().parent.parent.parent
-            logs_dir = basedir / "logs"
-            log_file = logs_dir / "server.log"
+            basedir = Path(__file__).resolve().parent.parent.parent # 项目根目录
+            logs_dir = basedir / "logs"  # 日志目录：项目根目录/logs
+            log_file = logs_dir / "server.log"  # 日志文件路径
 
-            logs_dir.mkdir(parents=True, exist_ok=True)
+            logs_dir.mkdir(parents=True, exist_ok=True) # 自动创建logs目录（若不存在）
 
-            logger = logging.getLogger('apitest')
-            if not logger.handlers:
-                logger.setLevel(logging.DEBUG)
+            logger = logging.getLogger('apitest')  # 创建名为'apitest'的日志器
+            if not logger.handlers:                # 避免重复添加处理器（防止日志重复输出）
+                logger.setLevel(logging.DEBUG)     # 日志器总级别（DEBUG，允许所有级别通过）
 
-                file_handler = setup_file_handler(log_file)
-                logger.addHandler(file_handler)
+                file_handler = setup_file_handler(log_file) # 添加文件处理器
+                logger.addHandler(file_handler)             # 添加控制台处理器
 
                 console_handler = setup_console_handler()
                 logger.addHandler(console_handler)
