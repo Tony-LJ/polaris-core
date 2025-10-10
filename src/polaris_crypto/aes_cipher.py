dffc2f6f2d48654d083a1d2b471678416a4bd7ff
tony <tony1@kinwong.com>
2025-10-10 16:15:23 +0800

    feat：AES对称加密算法

---
+++ src/polaris_crypto/aes_cipher.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+descr : AES对称加密算法
+auther : lj.michale
+create_date : 2025/10/10 15:54
+file_name : AESCipher.py
+"""
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+import os
+
+
+class AESCipher:
+    def __init__(self, key):
+        self.key = key.encode()  # 密钥必须是字节类型
+        self.backend = default_backend()
+
+    def _pad(self, data):
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(data) + padder.finalize()
+        return padded_data
+
+    def _unpad(self, data):
+        unpadder = padding.PKCS7(128).unpadder()
+        unpadded_data = unpadder.update(data) + unpadder.finalize()
+        return unpadded_data
+
+    def encrypt(self, plaintext):
+        iv = os.urandom(16)  # 初始化向量
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=self.backend)
+        encryptor = cipher.encryptor()
+        padded_data = self._pad(plaintext.encode())
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+        return urlsafe_b64encode(iv + ciphertext).decode()  # 返回base64编码的字符串，便于传输和存储
+
+    def decrypt(self, ciphertext):
+        ciphertext = urlsafe_b64decode(ciphertext)
+        iv = ciphertext[:16]  # 提取初始化向量
+        ciphertext = ciphertext[16:]  # 提取密文数据
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=self.backend)
+        decryptor = cipher.decryptor()
+        decrypted_padded_data = decryptor.update(ciphertext) + decryptor.finalize()
+        return self._unpad(decrypted_padded_data).decode()  # 返回解密后的明文
+
+
--- tests/polaris_common/polaris_common_unit_test.py
+++ tests/polaris_common/polaris_common_unit_test.py
@@ -8,4 +8,3 @@ if __name__ == '__main__':
     status, response_text = client.get('https://h.moyanjdc.top', None)
     print(status)  # 打印HTTP状态码
     print(response_text)  # 打印响应体内容（对于requests，不需要手动关闭连接）
-    # print(http_client.HttpClient.get("GET","https://h.moyanjdc.top",None))
