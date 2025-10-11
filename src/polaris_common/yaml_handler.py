f03bfd271f4d22bc4e9764d806cc529fb2f2b74a
tony <tony1@kinwong.com>
2025-10-11 09:30:26 +0800

    feat：操作YAML的工具类

--- src/polaris_common/yaml_handler.py
+++ src/polaris_common/yaml_handler.py
@@ -10,3 +10,24 @@ file_name : yaml_handler.py
 import os.path
 import yaml
 
+
+class YamlUtil:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            print("YamlUtil first init")
+            cls.__instance = super(YamlUtil, cls).__new__(cls, *args, **kwargs)
+        return cls.__instance
+
+    def read_yaml(self, path):
+        with open(path, encoding="utf-8") as f:
+            result = f.read()
+            result = yaml.load(result, Loader=yaml.FullLoader)
+            return result
+
+    def write_yaml(self, path, data):
+        with open(path, "w", encoding="utf-8") as f:
+            yaml.dump(data, f, Dumper=yaml.SafeDumper)
+
+yamlUtil = YamlUtil()
