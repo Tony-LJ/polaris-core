a3d671429f6bcbe2f22fbee7527fd5f3184879d2
tony <tony1@kinwong.com>
2025-10-11 09:35:55 +0800

    feat：操作YAML的工具类

--- tests/polaris_common/polaris_common_unitest.py
+++ tests/polaris_common/polaris_common_unitest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from polaris_common import http_client
+from polaris_common import yaml_handler
 
 
 if __name__ == '__main__':
@@ -8,3 +9,7 @@ if __name__ == '__main__':
     status, response_text = client.get('https://h.moyanjdc.top', None)
     print(status)  # 打印HTTP状态码
     print(response_text)  # 打印响应体内容（对于requests，不需要手动关闭连接）
+
+    print(yaml_handler.yamlUtil.read_yaml("D:\\project\\polaris-core\\docs\\application-prod.yaml"))
+
+
