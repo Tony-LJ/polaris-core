0d941687a0d708075cf38c5d2cb316a209204b21
tony <tony1@kinwong.com>
2025-10-11 09:39:59 +0800

    feat：polaris_logger日志模块调用测试

--- tests/polaris_common/polaris_common_unitest.py
+++ tests/polaris_common/polaris_common_unitest.py
@@ -2,7 +2,7 @@
 
 from polaris_common import http_client
 from polaris_common import yaml_handler
-
+from polaris_logger import logger
 
 if __name__ == '__main__':
     client = http_client.HttpClient()
@@ -11,5 +11,6 @@ if __name__ == '__main__':
     print(response_text)  # 打印响应体内容（对于requests，不需要手动关闭连接）
 
     print(yaml_handler.yamlUtil.read_yaml("D:\\project\\polaris-core\\docs\\application-prod.yaml"))
+    logger.info(yaml_handler.yamlUtil.read_yaml("D:\\project\\polaris-core\\docs\\application-prod.yaml"))
 
 
