deb7692f460c28c79754df6e25834bf0e503f011
tony <tony1@kinwong.com>
2025-10-18 16:09:12 +0800

    feat:MysqlClient单元测试

--- test/polaris_connector/polaris_mysql_unitest.py
+++ test/polaris_connector/polaris_mysql_unitest.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from polaris_connector.mysql import MysqlClient
+
+# 初始化连接池
+pool = MysqlClient(
+    host='10.53.0.71',
+    database='bigdata',
+    user='root',
+    password='LJkwhadoop2025!',
+    port=3306
+)
+
+if __name__ == '__main__':
+    table_output = f"ODS_FND_FLEX_VALUE_SETS"
+    data = pool.get("SELECT * FROM dask_ods_meta WHERE table_output=%s", (table_output,))
+    print(data)
+
+
