5689d919bf0bfeb939a257035979d8a2e10001c0
tony <tony1@kinwong.com>
2025-10-20 11:50:48 +0800

    feat:mqtt|MQTT订阅消息

--- src/polaris_mqtt/mqtt_subscriber.py
+++ src/polaris_mqtt/mqtt_subscriber.py
@@ -9,6 +9,7 @@ file_name: mqtt_subscriber.py
 import paho.mqtt.client as mqtt
 import json
 
+
 class MQTTSubscriber:
     """
     MQTT订阅消息的类
@@ -29,6 +30,9 @@ class MQTTSubscriber:
         # 设置自动重连的时间间隔，最小为 1 秒，最大为 120 秒
         self.client.reconnect_delay_set(min_delay=1, max_delay=120)
 
+        # 初始化消息
+        self.message = None
+
     def on_connect(self, client, userdata, flags, rc):
         """连接成功回调函数"""
         # 打印连接结果，0 表示成功，其他值表示失败
@@ -38,24 +42,28 @@ class MQTTSubscriber:
         self.client.subscribe(self.topic, self.qos)
 
     def on_message(self, client, userdata, msg):
-        """
-        订阅消息回调函数，当收到订阅的消息时被调用
-        :param client: MQTT客户端实例
-        :param userdata: 用户自定义数据
-        :param msg: 收到的消息对象
-        """
+        """订阅消息回调函数，当收到订阅的消息时被调用"""
         try:
             # 将消息中的JSON字符串转换为Python字典对象
-            message = json.loads(msg.payload.decode('gb2312'))
+            self.message = json.loads(msg.payload.decode('gb2312'))
         except ValueError:
             # 如果无法转换为Python字典对象，则直接使用字符串格式
-            message = msg.payload.decode('gb2312')
+            self.message = msg.payload.decode('gb2312')
 
         # 打印收到的消息的主题和内容
-        print(f"主题: {msg.topic} 消息: {message}")
+        print(f"主题: {msg.topic} 消息: {self.message}")
+
+    def get_message(self):
+        """获取最新的消息"""
+        return self.message
 
     def subscribe(self, topic, qos=0):
-        """连接 MQTT 服务器，并订阅消息"""
+        """
+        连接 MQTT 服务器，并订阅消息
+        :param topic:
+        :param qos:
+        :return:
+        """
         # 订阅的主题
         self.topic = topic
         # 消息质量等级
@@ -66,8 +74,18 @@ class MQTTSubscriber:
         self.client.on_message = self.on_message
         # 连接MQTT服务器，keepalive为60秒，表示每隔60秒发送一次心跳包
         self.client.connect(self.host, self.port, keepalive=60)
-        # 使用loop_forever()方法保持连接并接收消息，直到断开连接或出现异常
-        self.client.loop_forever()
+        # 启动一个新的线程来处理网络交互
+        self.client.loop_start()
+
+    def disconnect(self):
+        """
+        断开 MQTT 服务器连接
+        :return:
+        """
+        # 断开连接
+        self.client.disconnect()
+        # 停止线程
+        self.client.loop_stop()
 
 
 # if __name__ == "__main__":
