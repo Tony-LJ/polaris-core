9041c29089a72b9ecd0d0fc0986ce062783b7f27
tony <tony1@kinwong.com>
2025-10-21 09:05:42 +0800

    feat:添加kw特有模块|发送数仓质检markdown报告

--- src/polaris_kw/kw_mardown_msg.py
+++ src/polaris_kw/kw_mardown_msg.py
@@ -9,22 +9,22 @@ file_name: kw_mardown_msg.py
 
 def send_dw_quality_markdown_msg(current_date,
                                  meta_list,
-                                 quality_error_lst,
+                                 quality_error_list,
                                  important_error_list,
-                                 wanzhengxing_results,
-                                 yizhixing_results,
-                                 zhujianweiyi_results,
-                                 zhunquexing_results):
+                                 wanzhengxing_list,
+                                 yizhixing_list,
+                                 zhujianweiyi_list,
+                                 zhunquexing_list):
     """
     发送数仓质检markdown报告
     :param current_date:
     :param meta_list:
-    :param quality_error_lst:
+    :param quality_error_list:
     :param important_error_list:
-    :param wanzhengxing_results:
-    :param yizhixing_results:
-    :param zhujianweiyi_results:
-    :param zhunquexing_results:
+    :param wanzhengxing_list:
+    :param yizhixing_list:
+    :param zhujianweiyi_list:
+    :param zhunquexing_list:
     :return:
     """
     report_content = f'''# **每日数仓质检报告**
@@ -32,10 +32,10 @@ def send_dw_quality_markdown_msg(current_date,
                          > **质检人**: <font color='black'> 大数据团队 </font>
                          > **质检规则库**: <font color='black'> bi_ods.dw_quality_check_rules </font>
                          > **质检规则数**: <font color='black'> {len(meta_list)} </font>
-                         > **质检异常数**: <font color='red'> {len(quality_error_lst)} </font> 
-                         > **质检得分**: <font color='green'> {round(((len(meta_list)-len(quality_error_lst))/len(meta_list)) * 100, 2)} </font> 
+                         > **质检异常数**: <font color='red'> {len(quality_error_list)} </font> 
+                         > **质检得分**: <font color='green'> {round(((len(meta_list)-len(quality_error_list))/len(meta_list)) * 100, 2)} </font> 
                          > **质检重要异常列表**: <font color='black'> {important_error_list} </font> 
-                         > **质检全部异常列表**: <font color='black'> 完整性异常:{wanzhengxing_results},\n 一致性异常:{yizhixing_results},\n 主键唯一异常:{zhujianweiyi_results},\n 准确性异常:{zhunquexing_results} </font> 
+                         > **质检全部异常列表**: <font color='black'> 完整性异常:{wanzhengxing_list},\n 一致性异常:{yizhixing_list},\n 主键唯一异常:{zhujianweiyi_list},\n 准确性异常:{zhunquexing_list} </font> 
     '''
 
     return report_content
