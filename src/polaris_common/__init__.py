e742e1cbc4ce5831845444cd04e7ea58abc787aa
tony <tony1@kinwong.com>
2025-10-23 15:04:40 +0800

    feat:通用日期格式

--- src/polaris_common/__init__.py
+++ src/polaris_common/__init__.py
@@ -3,10 +3,12 @@
 from .http_client import HttpClient
 from .common_constant import ch_month_name
 from .common_constant import ch_week_name
-
+from .common_constant import DatetimeFormat
 
 __all__ = [
     'HttpClient',
     'ch_month_name',
     'ch_week_name',
+    'DatetimeFormat',
 ]
+
--- src/polaris_common/common_constant.py
+++ src/polaris_common/common_constant.py
@@ -6,6 +6,9 @@ auther: lj.michale
 create_date: 2025/9/27 15:54
 file_name: common_constant.py
 """
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36'
+
 # 中文月份名称
 ch_month_name = [
     "一月",
@@ -25,7 +28,25 @@ ch_month_name = [
 # 中文周名称
 ch_week_name = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
 
+class DatetimeFormat:
+    """
+    通用日期格式
+    """
+    DISPLAY_MONTH = '%Y-%m'
+    DISPLAY_DATE = '%Y-%m-%d'
+    DISPLAY_DT = '%Y-%m-%d %H:%M:%S'
 
+    SUFFIX_DT = '%Y%m%d%H%M%S'
+    SUFFIX_DT_UNDERLINE = '%Y_%m_%d_%H_%M_%S'
+
+    SUFFIX_DATE = '%Y%m%d'
+    SUFFIX_DATE_UNDERLINE = '%Y_%m_%d'
+
+    SUFFIX_MONTH = '%Y%m'
+    SUFFIX_MONTH_UNDERLINE = '%Y_%m'
+
+    SUFFIX_YEAR = '%Y'
+    SUFFIX_YEAR_UNDERLINE = '%Y'
 
 
 
