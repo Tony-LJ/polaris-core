bfc784b8fa9cef616d6deed3093ebc035d430c47
tony <tony1@kinwong.com>
2025-10-24 09:57:38 +0800

    feat:通用工具|將JSON深度嵌套的结构展平为二维表格

---
+++ src/polaris_common/json_utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+descr: JSON操作工具类
+auther: lj.michale
+create_date: 2025/9/27 15:54
+file_name: json_utils.py
+"""
+import pandas as pd
+
+
+class JsonUtils:
+
+    @staticmethod
+    def json_to_df(json_data):
+        """
+         將JSON深度嵌套的结构展平为二维表格
+        :param self:
+        :param json_data:
+        :param sep:
+        :return:
+        """
+        df = pd.json_normalize(json_data, sep="_")
+
+        return df
+
+
+
+
+
+
+if __name__ == '__main__':
+    data = [
+        {"id": 1, "info": {"name": "Alice", "age": 30}, "hobbies": ["reading", "cycling"]},
+        {"id": 2, "info": {"name": "Bob", "age": 25}, "hobbies": ["gaming"]}
+    ]
+
+    df = JsonUtils.json_to_df(data)
+    print(df)
