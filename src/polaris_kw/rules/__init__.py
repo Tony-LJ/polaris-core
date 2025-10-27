2bb38f3933b9104e167bb14804d3565482f0b275
tony <tony1@kinwong.com>
2025-10-27 11:40:45 +0800

    feat:計算电路板产品类型

---
+++ src/polaris_kw/rules/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .manufacture_business_rules import manufacture_business_rules
+
+__all__ = [
+    'manufacture_business_rules'
+]
--- src/polaris_kw/rules/manufacture_business_rules.py
+++ src/polaris_kw/rules/manufacture_business_rules.py
@@ -7,3 +7,29 @@ create_date: 2025/9/27 15:54
 file_name: manufacture_business_rules.py
 """
 
+
+class manufacture_business_rules:
+
+    @staticmethod
+    def calc_product_type(cust_code,
+                          bus_factory,
+                          product_group,
+                          hdi_degree,
+                          item_level,
+                          product_sut,item_category,borad_flg,lot_color):
+        """
+        計算电路板产品类型
+        :param cust_code:
+        :param bus_factory:
+        :param product_group:
+        :param hdi_degree:
+        :param item_level:
+        :param product_sut:
+        :param item_category:
+        :param borad_flg:
+        :param lot_color:
+        :return: product_type
+        """
+        product_type = ""
+
+        return product_type
