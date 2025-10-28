e4796f643421bfcbf979a86d2be76446c21ee31c
tony <tony1@kinwong.com>
2025-10-28 10:24:08 +0800

    feat:日期处理|时区转换计算

--- src/polaris_common/datetime_utils.py
+++ src/polaris_common/datetime_utils.py
@@ -6,6 +6,7 @@ create_date: 2025/9/19 17:11
 file_name: datetime_utils.py
 """
 from datetime import datetime
+import pytz
 
 
 def get_current_time(format):
@@ -23,6 +24,27 @@ def get_current_time(format):
 
     return formatted_time
 
+def get_datetime_timezone(iso_str, time_zone, format):
+    """
+     时区转换计算
+    :param iso_str:
+    :param time_zone: Asia/Shanghai
+    :param format: %Y-%m-%d, %H:%M:%S %Z
+    :return:
+    """
+    # 解析ISO 8601字符串并设置为UTC时区
+    utc_dt = ''
+    if iso_str is None:
+        utc_dt = datetime.fromisoformat(str("2099-10-05T14:30:00")).replace(tzinfo=pytz.utc)
+    else:
+        utc_dt = datetime.fromisoformat(str(iso_str)).replace(tzinfo=pytz.utc)
+    # 定义目标时区（例如CST，即中国标准时间）
+    cst_tz = pytz.timezone(time_zone)
+    # 将UTC时间转换为CST时间
+    cst_dt = utc_dt.astimezone(cst_tz)
+    formatted_datetime_str = cst_dt.strftime(format)
+
+    return formatted_datetime_str
 
 
 if __name__ == '__main__':
