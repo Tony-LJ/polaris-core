82563c23671a027165c3355598c06f18a974786d
tony <tony1@kinwong.com>
2025-10-29 16:50:56 +0800

    feat:生成Hive MetaStore维度表

---
+++ src/polaris_dw/dim_device_dataset.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""
+descr: 生成设备维度表
+auther: lj.michale
+create_date: 2025/10/28 15:54
+file_name: dim_device_dataset.py
+"""
+
+from polaris_common.datetime_utils import get_current_time
+import uuid
+import pandas as pd
+
+def create_structured_dim_device():
+    """
+    生成设备维度表schema
+    :return:
+    """
+    structured_dim_device = {}
+    structured_dim_device["id"] = uuid.uuid4()
+
+    # etl计算日
+    structured_dim_device["etl_date"] = get_current_time("%Y-%m-%d %H:%M:%S")
+
+    return structured_dim_device
+
+def generate_dim_device_dataset():
+    """
+    生成真实数仓设备维度表
+    :return:
+    """
+    datas = []
+
+    for data in datas:
+        datas.append(create_structured_dim_device(data))
+
+    return pd.DataFrame(datas)
+
+
+if __name__ == '__main__':
+    print(generate_dim_device_dataset().to_string())
+
