f4f8d06d9b8bc4b4c2e50e0da47cabf5e43dee6b
tony <tony1@kinwong.com>
2025-10-29 16:32:26 +0800

    feat:数仓常用通用维度表

--- src/polaris_dw/__init__.py
+++ src/polaris_dw/__init__.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from .dim_date_dataset import generate_dim_date_dataset
-
+from .dim_region_dataset import generate_dim_region_dataset
+from .dim_customer_dataset import generate_dim_customer_dataset
+from .dim_product_dataset import generate_dim_product_dataset
+from .dim_ledger_account_dataset import generate_dim_ledger_account_dataset
 
 __all__ = [
     'generate_dim_date_dataset',
+    'generate_dim_region_dataset',
+    'generate_dim_customer_dataset',
+    'generate_dim_product_dataset',
+    'generate_dim_ledger_account_dataset',
 ]
