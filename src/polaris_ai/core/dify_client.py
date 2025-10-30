7fdf2634dde2e66d8f6e42009f635b456957e30e
tony <tony1@kinwong.com>
2025-10-30 16:31:56 +0800

    feat:Dify client客户端

---
+++ src/polaris_ai/core/dify_client.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+descr: Dify client客户端
+auther: lj.michale
+create_date: 2025/10/18 09:54
+file_name: dify_client.py
+"""
+
+
+class DifyClient:
+
+    def __init__(self):
+        print(">>>>>>>>>>>>>")
