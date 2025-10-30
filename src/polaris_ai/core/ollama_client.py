16d677e9d73bddb2dd1cbc599aaf26163a9b3272
tony <tony1@kinwong.com>
2025-10-30 16:23:12 +0800

    feat:ollama client客户端

---
+++ src/polaris_ai/core/ollama_client.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+descr: ollama client客户端
+auther: lj.michale
+create_date: 2025/10/18 09:54
+file_name: ollama_client.py
+"""
+
+
+class OllamaClient:
+
+    def __init__(self):
+        print(">>>>>>>>>>>>>")
