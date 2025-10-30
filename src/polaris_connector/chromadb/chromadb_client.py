a3f6c4119307420652eaf35cae42e601e9183625
tony <tony1@kinwong.com>
2025-10-30 16:29:08 +0800

    feat:Chromadb客户端

---
+++ src/polaris_connector/chromadb/chromadb_client.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+descr: Chromadb客户端
+auther: lj.michale
+create_date: 2025/10/18 09:54
+file_name: ollama_client.py
+"""
+
+
+class ChromadbClient:
+    def __init__(self):
+        print("ChromaDB：一个向量数据库，用于存储和管理我们的数据嵌入")
